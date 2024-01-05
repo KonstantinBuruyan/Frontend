bb609a19e2ea477cb86bab2ba1fc4f4ab27c0953
Konstantin Buruyan <k.burujan@gmail.com>
2024-01-05 21:08:26 +0200

    Lectures 05.01 JavaScript

---
+++ Lectures/JavaScript/variables.js
@@ -0,0 +1,76 @@
+// let a;
+// a=10;
+
+// let b=20;
+// console.log(a);
+
+// a=20;
+// console.log(a);
+
+// const c = 100;
+
+
+// // simple comment
+// // ! sdsdsds
+// // ? sxax
+
+// let num= 123.023;
+// console.log(typeof num);
+
+// console.log(0.1+0.2);
+
+
+// console.log(120+40);
+
+// console.log(120-40);
+
+// console.log(120*40);
+
+// console.log(120/40);
+
+// console.log(120**40);
+
+// console.log(9%4);
+
+// let x=5;
+// x+=5;
+
+// console.log(x);
+
+let userName ="Jack Smith";
+
+// let userName2 = userName;
+
+// userName2 +=   "new ";
+
+// console.log( userName2);
+
+// console.log(typeof userName);
+// console.log(userName+ " Hello word");
+
+
+// console.log("My\t favourite\n book is \"Metro 2033\"");
+
+// let text = "hello\n";
+
+// console.log(text.length);
+
+// let name = "Roman";
+// let surname = "Kutselepa";
+
+// console.log(`Name: ${name} Surname: ${surname}`);
+
+// null
+// undefined
+// NaN
+
+let bool=true;
+
+let x = Boolean(-0); // false return only 0 or -0 , NaN
+console.log(x);
+
+let a = Boolean(NaN);
+console.log(a);
+
+let s = Boolean("Hello world"); // false return only empty string
+console.log(s);

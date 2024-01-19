405c47766f00a348bf20fc126e85c41b3444e6d3
Konstantin Buruyan <k.burujan@gmail.com>
2024-01-19 21:07:10 +0200

    Lectures 19.01. NPM

---
+++ Lectures/npm_test/libs.js
@@ -0,0 +1,12 @@
+const today = new Date("2020-03-12 16:12:32");
+console.log(today);
+
+// const moment = require("moment");
+
+// console.log(moment().format("MM"));
+
+// import moment from "moment";
+
+// console.log(moment.format());
+
+const _ = require("lodash");
